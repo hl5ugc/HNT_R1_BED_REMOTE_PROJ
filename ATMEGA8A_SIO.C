d6739ba6bfc82c2cb4ee5705d25597109b0be21a
Hag Seong Kang <hl5ugc@nate.com>
2022-11-29 19:06:41 +0900

    First Files

---
+++ ATMEGA8A_SIO.C
@@ -0,0 +1,377 @@
+// =======================================================================
+// [ PROJECT   ]
+// -----------------------------------------------------------------------
+// [ License   ] SAMJIN ELECTRONICS
+// [ Author    ] Copyright 2021.8 By HAG-SEONG KANG (hl5ugc@nate.com)
+// [ HomePage  ]
+// [ C  P  U   ]
+// [ Compller  ] CodeWizardAVR V13.12 Professional
+// [ Filename  ] ATMEGA8A_SIO.C
+// [ Version   ] 0.5
+// [ Created   ] 2022/09/08
+// [ Modified  ]
+// ------------------------------------------------------------------
+// [Updata  List] :
+//
+//
+//
+//
+//
+// ==================================================================
+//
+//   
+//  
+//
+#pragma used+
+#include <mega8.h>
+#include <delay.h>
+#include "uart.h" 
+ 
+// ---------------------------------------------------------------------------
+// typedef define
+// --------------------------------------------------------------------------- 
+ 
+// ---------------------------------------------------------------------------
+// Structure and Union Definitions
+// ---------------------------------------------------------------------------        
+// ---------------------------------------------------------------------------
+// UART defines
+// ---------------------------------------------------------------------------
+// 
+#define STX         '@'
+#define ETX         '$'
+//
+#define PC_STX      '@' 
+#define PC_ETX      '$' 
+#define PC_ACK      '#'
+#define ALL_ADD     '?' 
+//
+
+#define OVR 3
+
+#define FRAMING_ERROR (1<<FE)
+#define PARITY_ERROR (1<<UPE)
+#define DATA_OVERRUN (1<<OVR)
+#define DATA_REGISTER_EMPTY (1<<UDRE)
+#define RX_COMPLETE (1<<RXC)
+//
+#define UART_TX_BUF_MAX     30
+#define UART_RX_BUF_MAX     30 
+//
+// --------------------------------------------------------------------------- 
+// UART Variables Define
+// ----------------------------------------------------------------------------
+// 
+vuint8_t Uart_Tx_Buff[UART_TX_BUF_MAX] ;
+uint8_t Uart_Tx_Wr_Ptr  ;
+uint8_t Uart_Tx_Rd_Ptr  ;
+//uint8_t Uart_Tx_Event   ;
+//uint8_t Uart_Tx_Count   ;
+//
+vuint8_t Uart_Rx_Buff[UART_RX_BUF_MAX] ;
+uint8_t Uart_Rx_Wr_Ptr  ;
+uint8_t Uart_Rx_Rd_Ptr  ;
+uint8_t Uart_Rx_Event   ;
+uint8_t Uart_Rx_Wait    ;
+//
+//
+// ---------------------------------------------------------------------------
+// Variables Define
+// --------------------------------------------------------------------------- 
+//
+REMOTE_FLAG_t        REMOTE_cmd1_flag ;
+vuint8_t PLC_DATA[PLC_DATA_MAX] ;
+
+// ---------------------------------------------------------------------------
+// Define Function prototypes.
+// --------------------------------------------------------------------------- 
+//
+static uint8_t  hex2byte_To_Bin(uint8_t u8Address) ;
+static void     send_Bin_2AsciiHex(uint8_t u8Data);
+static void     send_AsciiByte(uint8_t u8Data) ;
+// ---------------------------------------------------------------------------
+// GLOBAL Function Routine Start
+// --------------------------------------------------------------------------- 
+//
+//   
+/** -------------------------------------------------------------------------------------
+ * @brief UART Input Data Parsing Routine
+ * 
+ * data Frame  STX ,CMD ,Mem,Count, HEX_ASCII (2Byte), ETX
+------------------------------------------------------------------------------------- */
+void Uart_Check(void)  
+{
+    static uint8_t  i_temp  = 0;
+    static uint8_t  address = 0;
+    static uint8_t  counter = 0; 
+    static uint16_t check = 0;
+    // 
+    if(Uart_Rx_Event == 1)          // Check Data Frame END
+    {
+        Uart_Rx_Event = 0 ;         //
+        if(Uart_Rx_Buff[0] == STX)  // Check Data Frame Start
+        { 
+            REMOTE_UART_OK_Flag_On ;
+            switch(Uart_Rx_Buff[1])
+            {
+                case 'W' :  // Memory Write Command
+                case 'w' :
+                            address =  Uart_Rx_Buff[2] & 0x0F ;        
+                            counter =  Uart_Rx_Buff[3] & 0x0F ;
+                            i_temp = address + counter ;
+                            if(i_temp <= PLC_DATA_MAX)
+                            {
+                                //
+                                for(i_temp=0; i_temp<counter; i_temp++)
+                                {
+                                    PLC_DATA[address+i_temp] = hex2byte_To_Bin(i_temp*2 + 4)  ; 
+                                }
+                            }                             
+                            break ;
+                case 'R' :  // Memory Read Command
+                case 'r' :
+                            address =  Uart_Rx_Buff[2] & 0x0F ;        
+                            counter =  Uart_Rx_Buff[3] & 0x0F ;
+                            i_temp = address + counter ;
+                            if(i_temp <= PLC_DATA_MAX)
+                            {
+                                send_1Byte('W');
+                                send_1Byte(Uart_Rx_Buff[2]);    // Address
+                                send_1Byte(Uart_Rx_Buff[3]);    // Counter
+                                
+                                for(i_temp=0; i_temp<counter; i_temp++)
+                                {
+                                    send_Bin_2AsciiHex(PLC_DATA[address+i_temp]);     
+                                }
+                            }      
+                            //
+                            send_1Byte(ETX); 
+                            send_STX() ;
+                            break ;            
+            
+                default :
+                            break ; 
+            }
+        }
+    }
+}
+
+void send_Write_Data_Bytes(uint8_t u8Address,uint8_t u8Count) 
+{
+    static uint8_t u8Temp  = 0;
+    while(Uart_Tx_Wr_Ptr != Uart_Tx_Rd_Ptr ) 
+    {
+        delay_ms(1) ;
+    } 
+    //
+    send_1Byte('W');
+    send_AsciiByte(u8Address);
+    send_AsciiByte(u8Count);   
+    //
+    for(u8Temp = 0; u8Temp < u8Count; u8Temp++)
+    {
+        send_Bin_2AsciiHex(PLC_DATA[u8Address+u8Temp]);
+    } 
+    send_1Byte(ETX);
+    // 
+    send_STX() ;
+  
+} 
+
+/** -------------------------------------------------------------------------------------
+ * @brief   Send 1byte Data To Uart Buffer
+ * 
+ * @param   u8data // send bayte
+ ------------------------------------------------------------------------------------- */
+void send_1Byte(uint8_t u8data)
+{   
+    if(Uart_Tx_Wr_Ptr < UART_TX_BUF_MAX)
+    {
+        Uart_Tx_Buff[Uart_Tx_Wr_Ptr] =  u8data  ;
+        if(++Uart_Tx_Wr_Ptr == UART_TX_BUF_MAX) Uart_Tx_Wr_Ptr = 0;  
+    }
+}
+
+/** -------------------------------------------------------------------------------------
+ * @brief   Send Uart Start Text Code
+ * 
+------------------------------------------------------------------------------------- */
+void send_STX(void)
+{                   
+    //Txd_output_enable ;
+    //delay_us(5);
+    UDR = STX ;
+} 
+// ---------------------------------------------------------------------------
+// Local Function Routine Start
+// --------------------------------------------------------------------------- 
+//
+/** -------------------------------------------------------------------------------------
+ * @brief   HEXASCII 2Byte Data to Byte Bin Data Convertion 
+ * 
+ * @param   u8Address Input Buffer HEXASCII 2Byte Data Start Address
+ * @return  uint8_t   1byte Convertion Data
+------------------------------------------------------------------------------------- */
+static uint8_t hex2byte_To_Bin(uint8_t u8Address)  
+{
+    static uint8_t  temp     = 0 ;
+    static uint8_t  cal_temp = 0 ;
+    // 
+    if(u8Address < PLC_DATA_MAX)
+    {
+        temp = Uart_Rx_Buff[u8Address] ;
+        if(temp < 0x3A) temp = temp - 0x30 ;
+        else temp = temp - 0x37 ;
+        cal_temp = temp << 4 ; 
+        cal_temp = cal_temp & 0xF0 ;
+        //
+        u8Address++ ;
+        temp = Uart_Rx_Buff[u8Address] ; 
+        if(temp < 0x3A) temp = temp - 0x30 ;
+        else temp = temp - 0x37 ;
+        cal_temp = cal_temp + temp   ; 
+    }
+    // 
+    return  cal_temp ;
+}
+/** -------------------------------------------------------------------------------------
+ * @brief   Bin  1byte Dta to 2Byte HexAscii Convert and Uart Buffer save for Send UART
+ * 
+ * @param   u8Data send Bin 1byte data 
+------------------------------------------------------------------------------------- */
+static void send_Bin_2AsciiHex(uint8_t u8Data) 
+{
+    static uint8_t i_Temp = 0 ; 
+    //
+    i_Temp = (u8Data >> 4) & 0x0F ;
+    if(i_Temp <= 9) send_1Byte(i_Temp + '0') ;
+    else            send_1Byte((i_Temp - 10) + 'A') ;
+    //
+    i_Temp =  u8Data & 0x0F ;
+    if(i_Temp <= 9) send_1Byte(i_Temp + '0') ;
+    else            send_1Byte((i_Temp - 10) + 'A') ;
+}
+/** ------------------------------------------------------------------------------------- 
+ * @brief   Send 1byte(0x00 ~ 0x0F) Bin Data to ASscii Data Convert and Send UART
+ * 
+ * @param   u8data Bin Data(0x00 ~ 0x0F) For Send
+-------------------------------------------------------------------------------------  */
+static void send_AsciiByte(uint8_t u8Data)
+{   
+    static uint8_t i_Temp = 0 ; 
+    //
+    i_Temp =  u8Data & 0x0F ;
+    if(i_Temp <= 9) send_1Byte(i_Temp + '0') ;
+    else            send_1Byte((i_Temp - 10) + 'A') ;
+}
+
+// ---------------------------------------------------------------------------
+// USART Receiver interrupt service routine
+// --------------------------------------------------------------------------- 
+//
+interrupt [USART_RXC] void uart_rx_isr(void)
+{
+
+    static uint8_t status = 0 ; 
+    static uint8_t data = 0 ;
+    //
+    status= UCSRA  ; 
+    data  = UDR  ; 
+    //
+    if(data == STX)
+    {
+        Uart_Rx_Wr_Ptr  = 0 ;
+        Uart_Rx_Event   = 0 ;
+    }
+    else if(data == ETX) 
+    {
+        Uart_Rx_Event   = 1 ;
+    }    
+    else ;
+    //-------------------------------------------------------------
+    if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN))==0)
+    {   
+        if(Uart_Rx_Wr_Ptr < UART_RX_BUF_MAX)
+        {
+            Uart_Rx_Buff[Uart_Rx_Wr_Ptr] = data ;
+            if (++Uart_Rx_Wr_Ptr >= UART_RX_BUF_MAX) Uart_Rx_Wr_Ptr = 0;         
+        }
+    } 
+    // 
+    Uart_Rx_Wait = 3 ; //
+} 
+// ---------------------------------------------------------------------------
+// USART Transmitter interrupt service routine
+// ---------------------------------------------------------------------------
+//
+interrupt [USART_TXC] void uart_tx_isr(void)
+{
+    if(Uart_Tx_Wr_Ptr != Uart_Tx_Rd_Ptr)
+    {             
+        if(Uart_Tx_Rd_Ptr < UART_TX_BUF_MAX) 
+        {
+            UDR = Uart_Tx_Buff[Uart_Tx_Rd_Ptr]; 
+            if(++Uart_Tx_Rd_Ptr >= UART_TX_BUF_MAX) Uart_Tx_Rd_Ptr = 0; 
+        }  
+    }  
+
+}
+// ---------------------------------------------------------------------------
+// Uart Port Init
+// --------------------------------------------------------------------------- 
+//
+void Uart_Init(void)
+{
+#if (USART_BPS == 9600)
+    // USART initialization
+    // Communication Parameters: 8 Data, 1 Stop, No Parity
+    // USART Receiver: On
+    // USART Transmitter: On
+    // USART Mode: Asynchronous
+    // USART Baud Rate: 9600
+//    UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
+//    UCSRB=(1<<RXCIE) | (1<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+//    UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
+//    UBRRH=0x00;
+//    UBRRL=0x64;     
+    // USART initialization   internal OSC 8Mhz
+    // Communication Parameters: 8 Data, 1 Stop, No Parity
+    // USART Receiver: On
+    // USART Transmitter: On
+    // USART Mode: Asynchronous
+    // USART Baud Rate: 9600
+    UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
+    UCSRB=(1<<RXCIE) | (1<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+    UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
+    UBRRH=0x00;
+    UBRRL=0x1A ;   // imsi
+#elif (USART_BPS == 19200)
+    // USART initialization
+    // Communication Parameters: 8 Data, 1 Stop, No Parity
+    // USART Receiver: On
+    // USART Transmitter: On
+    // USART Mode: Asynchronous
+    // USART Baud Rate: 19200
+    UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
+    UCSRB=(1<<RXCIE) | (1<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+    UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
+    UBRRH=0x00;
+    UBRRL=0x33;
+#elif (USART_BPS == 38400)
+    // USART initialization
+    // Communication Parameters: 8 Data, 1 Stop, No Parity
+    // USART Receiver: On
+    // USART Transmitter: On
+    // USART Mode: Asynchronous
+    // USART Baud Rate: 19200
+    UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
+    UCSRB=(1<<RXCIE) | (1<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+    UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
+    UBRRH=0x00;
+    UBRRL=0x19;
+#endif    
+
+}
+#pragma used-
+
+
---
+++ R1_BED_REMOTE.c
@@ -0,0 +1,206 @@
+//
+// =======================================================================
+// [ PROJECT   ] R1 BED REMOTE CONTROL 
+// -----------------------------------------------------------------------
+// [ License   ] SAMJIN ELECTRONICS
+// [ Author    ] Copyright 2022-09 By HAG-SEONG KANG
+// [ E-MAIL    ] hl5ugc@nate.com (82)10- 3841-9706
+// [ C  P  U   ] ATmega8A
+// [ Compller  ] CodeWizardAVR V3.12 Professional
+// [ Filename  ] R1_BED_REMORE.C
+// [ Version   ] 1.0
+// [ Created   ] 2022-09-10
+// ----------------------------------------------------------------------------
+// Revision History :
+// ----------------------------------------------------------------------------
+// Author         Date          Comments on this revision    
+// ----------------------------------------------------------------------------
+// HAG-SEONG KANG 2022-09-08    First release of Source file   
+// 
+// 
+// ============================================================================
+//
+//
+//
+//
+#include <mega8.h>
+#include <delay.h>
+// Standard Input/Output functions
+#include <stdio.h>
+// Declare your global variables here
+#include "SJTYPE.H"  
+#include "R1_BED_REMORE.H"
+#include "UART.H"  
+// --------------------------------------------------
+// Timer 0 overflow interrupt service routine
+// --------------------------------------------------
+interrupt [TIM0_OVF] void timer0_ovf_isr(void)
+{
+    // Reinitialize Timer 0 value
+    TCNT0=0x06; //  1mSec 
+    // Place your code here
+    Uart_Check() ;
+    //
+    // Buzzer Control
+    if(Is_Buzzer_Start_Flag)
+    {
+        Buzzer_On ; 
+        if(--Buzzer_On_time == 0x00)
+        {
+            Buzzer_Off ; 
+            Buzzer_Start_Flag_Off ;
+            Buzzer_End_Flag_On ;
+        }
+    }
+    else 
+    {   // Check Eror Mask
+        Buzzer_Off ;
+    } 
+    // 
+}
+// --------------------------------------------------
+// Timer 2 output compare interrupt service routine
+// --------------------------------------------------
+// 4mSec
+interrupt [TIM2_COMP] void timer2_comp_isr(void)
+{
+    // Place your code here
+    static uint8_t u8Data = 0 ;  
+    static uint16_t u16Data = 0 ;
+    //
+    // Key InPut    
+    u8Data =  ((~PINC) & 0b00000111) ;      // Joy s/w, sw10, sw9  
+    u16Data = ((uint16_t)u8Data << 8) & 0x0700 ;
+    //
+    // LEFT X > 128 , RIGHT X < 56
+    if(adc_data[0]      <= AD_SWITCH_MIN)        { u16Data |=  0x2000 ; }  // RIGHT X
+    else if(adc_data[0] >= AD_SWITCH_MAX)   { u16Data |=  0x1000 ; }  // LEFT X
+    else                                    { u16Data &=  0xCFFF ; }
+    // UP Y < 56 , DOWN Y > 200
+    if(adc_data[1]      <= AD_SWITCH_MIN)        { u16Data |=  0x4000 ; }    // UP Y
+    else if(adc_data[1] >= AD_SWITCH_MAX)   { u16Data |=  0x8000 ; }    // DOWN Y
+    else                                    { u16Data &=  0x3FFF ; } 
+    //
+    u8Data  = ((~PINB) & 0b00000111) ;
+    u8Data |= ((~PIND) & 0b11111000) ; 
+    u16Data =  u16Data + u8Data  ;  
+    // Check New Data
+    if(key_in_data_old != u16Data) 
+    {
+        key_in_data_old = u16Data ;
+    } 
+    // Re Check New Data 
+    else if(key_in_data != key_in_data_old)
+    {
+        key_in_data = key_in_data_old ; 
+        //   
+        Key_Change_Flag_On ;
+        // Check Key Pressed
+        if(key_in_data == 0x00)
+        {   // UnPress
+            Buzzer_End_Flag_Off ;
+        } 
+        else
+        {   // Check Key Pressed or New Key Pressed 
+            if(Is_Not_Buzzer_End_Flag)
+            {
+                Buzzer_On ; 
+                Buzzer_Start_Flag_On ;  
+                Buzzer_End_Flag_Off ;  
+                Buzzer_On_time = BUZZER_ON_TIME ;   
+            }
+        }
+    } 
+    else  // Key Data Update
+    {
+        KEY_DATA_BUFF2 = (key_in_data >> 8);  
+        KEY_DATA_BUFF1 =  key_in_data ;  
+        //
+        if(Is_Key_Change_Flag) {
+            Key_Change_Flag_Off ;
+            Key_Update_Flag_On ;
+        }
+    }
+    // Led Output Control   
+    hc595_Output(LED_DATA_BUFF) ;
+}
+// --------------------------------------------------
+// ADC interrupt service routine
+// with auto input scanning
+// --------------------------------------------------
+interrupt [ADC_INT] void adc_isr(void)
+{
+    static unsigned char input_index=0;
+    // Read the 8 most significant bits
+    // of the AD conversion result
+    adc_data[input_index]=ADCH;
+    // Select next ADC input
+    if (++input_index > (LAST_ADC_INPUT-FIRST_ADC_INPUT))
+       input_index=0;
+    ADMUX=(FIRST_ADC_INPUT | ADC_VREF_TYPE)+input_index;
+    // Delay needed for the stabilization of the ADC input voltage
+    delay_us(10);
+    // Start the AD conversion
+    ADCSRA|=(1<<ADSC);
+}
+
+/**
+ * @brief   Main Program Start
+ * 
+ */
+void main(void)
+{
+    static uint8_t u8Temp = 0 ;
+    // Declare your local variables here
+    System_Init();
+    Uart_Init() ;
+    // Global enable interrupts
+    #asm("sei")
+
+    while (1)
+    {
+        // key Data Update By Key Press or unPress 
+        if(Is_Key_Update_Flag) {
+            Key_Update_Flag_Off ; 
+            // Key Data Send To ACTUATOR Main Board
+            send_Write_Data_Bytes(0x00,0x02) ;
+        }  
+    }
+}
+//
+// -----------------------------------------------
+/**
+ * @brief   74HC595 For Led On/Off Control
+ * 
+ * @param   u8Data LED On/Off Data
+ */
+#define hc595_Clk_Hi    PORTB.5 = 1
+#define hc595_Clk_Lo    PORTB.5 = 0
+#define hc595_Data_Hi   PORTB.3 = 1
+#define hc595_Data_Lo   PORTB.3 = 0
+#define hc595_Cs_Hi     PORTC.5 = 1
+#define hc595_Cs_Lo     PORTC.5 = 0
+//
+static void hc595_Output(uint8_t u8Data)
+{
+    static uint8_t u8Temp = 0 ;
+    //
+    hc595_Clk_Hi ;
+    hc595_Data_Hi ; 
+    hc595_Cs_Lo ;
+    //
+    for(u8Temp = 8 ; u8Temp != 0 ; u8Temp--)
+    {
+        hc595_Clk_Lo ; 
+        if((u8Data & 0x80) == 0x00) { hc595_Data_Lo ;}
+        else { hc595_Data_Hi ;}
+        //
+        delay_us(2) ;
+        hc595_Clk_Hi ; 
+        delay_us(4) ;
+        //
+        u8Data = u8Data << 1 ;
+    } 
+    //  
+    hc595_Cs_Hi ;
+}
